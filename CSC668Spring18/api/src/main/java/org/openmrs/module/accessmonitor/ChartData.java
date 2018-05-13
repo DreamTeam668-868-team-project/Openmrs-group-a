71926a9796ef69748b3a26f5842ba43e0c384cc9
Travis <leibhaftige@hotmail.com>
2018-05-13 10:22:43 -0700

    add getters and setters for new end time value

--- CSC668Spring18/api/src/main/java/org/openmrs/module/accessmonitor/ChartData.java
+++ CSC668Spring18/api/src/main/java/org/openmrs/module/accessmonitor/ChartData.java
@@ -13,84 +13,94 @@ import org.openmrs.BaseOpenmrsObject;
  * @author guanming
  */
 public class ChartData extends BaseOpenmrsObject implements Serializable {
-
-    private Integer id;
-
-    private Date start;
-
-    private Date end;
-
-    private Integer number;
-
-    private Integer userId;
-
-    private String userGiven;
-
-    private String userFamily;
-
-    public ChartData(Date s, Date e, Integer n) {
-        start = s;
-        end = e;
-        number = n;
-    }
-
-    public ChartData(Integer userId, String userGiven, String userFamily,
-            Date s, Date e, Integer n) {
-        this.userId = userId;
-        this.userGiven = userGiven;
-        this.userFamily = userFamily;
-        start = s;
-        end = e;
-        number = n;
-    }
-
-    @Override
-    public Integer getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Date getTime() {
-        return start;
-    }
-
-    public void setTime(Date time) {
-        this.start = time;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getUserGiven() {
-        return userGiven;
-    }
-
-    public void setUserGiven(String userGiven) {
-        this.userGiven = userGiven;
-    }
-
-    public String getUserFamily() {
-        return userFamily;
-    }
-
-    public void setUserFamily(String userFamily) {
-        this.userFamily = userFamily;
-    }
+	
+	private Integer id;
+	
+	private Date start;
+	
+	private Date end;
+	
+	private Integer number;
+	
+	private Integer userId;
+	
+	private String userGiven;
+	
+	private String userFamily;
+	
+	public ChartData(Date s, Date e, Integer n) {
+                userId = 0;
+                userGiven = "";
+                userFamily = "";
+		start = s;
+		end = e;
+		number = n;
+	}
+	
+	public ChartData(Integer userId, String userGiven, String userFamily, Date s, Date e, Integer n) {
+		this.userId = userId;
+		this.userGiven = userGiven;
+		this.userFamily = userFamily;
+		start = s;
+		end = e;
+		number = n;
+	}
+	
+	@Override
+	public Integer getId() {
+		return id;
+	}
+	
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Date getStart() {
+		return start;
+	}
+	
+	public void setStart(Date time) {
+		this.start = time;
+	}
+        
+        public Date getEnd() {
+		return end;
+	}
+	
+	public void setEnd(Date time) {
+		this.end = time;
+	}
+	
+	public Integer getNumber() {
+		return number;
+	}
+	
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	public String getUserGiven() {
+		return userGiven;
+	}
+	
+	public void setUserGiven(String userGiven) {
+		this.userGiven = userGiven;
+	}
+	
+	public String getUserFamily() {
+		return userFamily;
+	}
+	
+	public void setUserFamily(String userFamily) {
+		this.userFamily = userFamily;
+	}
 }
